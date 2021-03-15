8b30a43d8a370a9808d761122949dd7ba725e80a
kireevmishka <kireev.mishka@gmail.com>
2021-03-16 02:11:43 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let brackets = [];
+    bracketsConfig.forEach((el) => {
+        brackets.push(el.join(""));
+    });
+    for (let i = 0; i < brackets.length; i++) {
+        if (str.includes(brackets[i])) {
+            str = str.replace(brackets[i], "");
+            i = -1;
+        }
+    }
+    return str.length === 0 ? true : false;
+};
